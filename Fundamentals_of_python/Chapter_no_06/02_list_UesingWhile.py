f4eabb382fe0ac302357b6ea75d50c679398bcc8
Husnena-Nadeem <husnena.nadeem.offical@gmail.com>
2025-10-19 07:57:20 -0700

    type of loops

---
+++ Fundamentals_of_python/Chapter_no_06/02_list_UesingWhile.py
@@ -0,0 +1,6 @@
+l=["CAT",1,2,"BIRDS","ANIMAL","HUMAN",253637]
+i=0
+
+while(i<len(l)):
+    print(l[i])
+    i+=1
---
+++ Fundamentals_of_python/Chapter_no_06/03_forLoop.py
@@ -0,0 +1,4 @@
+#This contant of for Loop:
+
+for i in range(0,100,4):
+    print(i)
