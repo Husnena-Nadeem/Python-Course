a5f643f1e6afa8966abe3c758a8e8d75046476c2
Husnena-Nadeem <husnena.nadeem.offical@gmail.com>
2025-10-19 09:04:03 -0700

    problem 1/2

---
+++ Fundamentals_of_python/Chapter_no_06/PS_Chapter_no_06/01.problem.py
@@ -0,0 +1,6 @@
+# print table using for loop
+
+n=int(input("Enter the Number:"))
+
+for i in range(1,11):
+    print(f"{n}x{i}={n*i}")
---
+++ Fundamentals_of_python/Chapter_no_06/PS_Chapter_no_06/02.problem.py
@@ -0,0 +1,7 @@
+#print list using loop
+l=["sana","sara","sonia","iram"]
+
+
+for name in l:
+    if(name.startswith("s")):
+        print(f"Hello {name}")
