b306a8039f2cdd1034b1d80ce65149ff65d1eaf6
Husnena-Nadeem <husnena.nadeem.offical@gmail.com>
2025-10-20 13:27:26 -0700

    functions

---
+++ Fundamentals_of_python/Chapter_no_07/01.Function .py
@@ -0,0 +1,8 @@
+def average():
+    a=int(input("Enter the number."))
+    b=int(input("Enter the number."))
+    c=int(input("Enter the number."))
+    average=a+b+c/3
+    print(f"average in {a} ,{b} and {c} = {average}")
+
+average()
