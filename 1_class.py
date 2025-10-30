4a016e155b415b85bdef398dacfccb5c8980f44b
Husnena-Nadeem <husnena.nadeem.offical@gmail.com>
2025-10-30 12:06:48 -0700

    class

--- 1_class.py
+++ 1_class.py
@@ -2,7 +2,7 @@ class person:
     name="Husnena"
     lenguage="Python"
 obj1=person()
-obj1.name="sana"
+obj1.name="sana"              #poriatey in instance atribuate in class atribute.
 
 print(obj1.name,obj1.lenguage)
 obj2=person()
