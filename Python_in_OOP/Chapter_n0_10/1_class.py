428d2ff85ae9ed95e52e89cc46385c06f02aa648
Husnena-Nadeem <husnena.nadeem.offical@gmail.com>
2025-10-31 09:20:20 -0700

    oop basic

--- Python_in_OOP/Chapter_n0_10/1_class.py
+++
@@ -1,9 +0,0 @@
-class person:
-    name="Husnena"
-    lenguage="Python"
-obj1=person()
-obj1.name="sana"              #poriatey in instance atribuate in class atribute.
-
-print(obj1.name,obj1.lenguage)
-obj2=person()
-print(obj2.name,obj1.lenguage)
