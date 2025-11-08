a2b40a23fc4f2e4e999eb8a4cb565b28184db739
Husnena-Nadeem <husnena.nadeem.offical@gmail.com>
2025-11-08 08:13:38 -0800

    target question

---
+++ DSA/test.py
@@ -0,0 +1,28 @@
+#Example no 01
+num=[2,7,11,15]
+target=9
+for i in range(len(num)):
+    current_sum=0
+    for j in range(i,len(num)):
+        current_sum+=num[j]
+        if current_sum==target:
+            print(f"Subarray from index {i} to {j}:",num[i:j+1])
+
+#Example no 02
+num=[3,2,4]
+target=6
+for i in range(len(num)):
+    current_sum=0
+    for j in range(i,len(num)):
+        current_sum+=num[j]
+        if current_sum==target:
+            print(f"Subarrat form index {i} to {j}:",num[i:j+1])
+
+#Example no 03
+num=[3,3]
+for i in range(len(num)):
+    current_sum=0
+    for j in range(i,len(num)):
+        current_sum+=num[j]
+        if current_sum==target:
+            print(f"subarrat for index {i} to {j}:" ,num[i:j+1])
