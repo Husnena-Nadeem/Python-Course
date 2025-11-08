3ffece6ac62cd36eb83af9e254e6325327689ea3
Husnena-Nadeem <husnena.nadeem.offical@gmail.com>
2025-11-08 01:29:59 -0800

    insertion

--- DSA/array.py
+++ DSA/array.py
@@ -6,6 +6,12 @@ for x in arr:
 arr=[4,2,7,8,1,2,5]
 arr.reverse()
 print("reverse:",arr)
+#insertion
+arr=[2,3,4,6]
+arr.append(4)
+print("Append",arr)
+arr.insert(2,10)
+print("insert",arr)
 
     
 
