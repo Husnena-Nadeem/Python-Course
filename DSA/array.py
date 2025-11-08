581997ae617f22d3ee88c41ca6a1571aeea6c09b
Husnena-Nadeem <husnena.nadeem.offical@gmail.com>
2025-11-08 03:37:01 -0800

    two pointer technique

--- DSA/array.py
+++ DSA/array.py
@@ -34,3 +34,12 @@ arr.sort()
 print(sorted(arr,reverse=True))
 
 
+#Two pointer technique
+arr=[1,2,3,4,5]
+print(len(arr))
+i,j=0,len(arr)-1
+while i<j:
+    arr[i],arr[j]=arr[j],arr[i]
+    i+=1
+    j-=1
+print(arr)
