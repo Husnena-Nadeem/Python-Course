742ab7d9aa8b80295d14e2519ea027028b189980
Husnena-Nadeem <husnena.nadeem.offical@gmail.com>
2025-11-08 23:15:42 -0800

    maximum and minimum element

---
+++ DSA/vect_pratic.py
@@ -0,0 +1,20 @@
+v=[]
+a1=int(input("Enter the value:"))
+v.append(a1)
+a2=int(input("Enter the value:"))
+v.append(a2)
+a3=int(input("Enter the value:"))
+v.append(a3)
+a4=int(input("Enter the value:"))
+v.append(a4)
+a5=int(input("Enter the value:"))
+v.append(a5)
+
+print(v)
+print(sum(v))
+
+#maximum and minimum element
+maximum=max(v)
+minimum=min(v)
+print("maximum number:",maximum)
+print("minimum number:",minimum)
