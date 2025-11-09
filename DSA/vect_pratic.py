8c9dce298ef850c307059687bb557bc8c2d073ae
Husnena-Nadeem <husnena.nadeem.offical@gmail.com>
2025-11-08 23:39:20 -0800

    find second largest element:

--- DSA/vect_pratic.py
+++ DSA/vect_pratic.py
@@ -20,14 +20,22 @@ minimum=min(v)
 print("maximum number:",maximum)
 print("minimum number:",minimum)
 
-#Reverse an array
+#Reverse an array:
 v1=[6,7,8,9]
 v1.reverse()
 print(v1)
-#count event numbers and odd numbers
+#count event numbers and odd numbers:
 v2=[1,8,9,67,6,2,8,19,10]
 for x in v2:
     if x%2==0:
         print(f"Number is even: {x}")
     else:
         print(f"Numbers is odd: {x}")
+
+#find second largest element:
+v3=[3,4,6,7,834,675,4]
+largest=max(v3)
+v3.remove(largest)
+second_largest=max(v3)
+print("Second largest number:",second_largest)   
+
