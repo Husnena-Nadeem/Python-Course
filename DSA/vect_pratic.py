a2bb960f478f1689c9e57474b15549c0ff385e4d
Husnena-Nadeem <husnena.nadeem.offical@gmail.com>
2025-11-08 23:23:20 -0800

    Reverse an array

--- DSA/vect_pratic.py
+++ DSA/vect_pratic.py
@@ -1,20 +1,27 @@
-v=[]
-a1=int(input("Enter the value:"))
-v.append(a1)
-a2=int(input("Enter the value:"))
-v.append(a2)
-a3=int(input("Enter the value:"))
-v.append(a3)
-a4=int(input("Enter the value:"))
-v.append(a4)
-a5=int(input("Enter the value:"))
-v.append(a5)
+# v=[]
+# a1=int(input("Enter the value:"))
+# v.append(a1)
+# a2=int(input("Enter the value:"))
+# v.append(a2)
+# a3=int(input("Enter the value:"))
+# v.append(a3)
+# a4=int(input("Enter the value:"))
+# v.append(a4)
+# a5=int(input("Enter the value:"))
+# v.append(a5)
 
-print(v)
-print(sum(v))
+# print(v)
+# print(sum(v))
 
 #maximum and minimum element
+v=[1,67,4900,364,-2,-1788]
 maximum=max(v)
 minimum=min(v)
 print("maximum number:",maximum)
 print("minimum number:",minimum)
+
+#Reverse an array
+v1=[6,7,8,9]
+v1.reverse()
+print(v1)
+#count event numbers and odd numbers
