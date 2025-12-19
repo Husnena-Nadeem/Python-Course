5c8706f67ee1bde53efd9e51af5d37bef022ef43
Husnena-Nadeem <husnena.nadeem.offical@gmail.com>
2025-12-19 05:52:10 -0800

    list

--- Fundamentals_of_python/pratic_set1.py
+++ Fundamentals_of_python/pratic_set1.py
@@ -5,3 +5,14 @@ print(name[1:4])
 print(name.capitalize())
 print(name.find("w"))
 print(name.endswith("e"))
+
+#list function
+name=[2,34,3,9,23,12,30,10,20,30]
+name.append(1000)
+print(name)
+name.sort()
+print(name)
+name.reverse()
+print(name)
+name.remove(2)
+print(name)
